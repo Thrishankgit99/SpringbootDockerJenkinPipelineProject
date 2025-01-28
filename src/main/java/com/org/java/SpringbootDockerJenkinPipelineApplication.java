00944cb71d0efe5235cc6537a3cb680bef815bb0
thrishankgit99 <thrishankgithub@gmail.com>
2025-01-28 16:26:25 +0530

    scxzxz

--- src/main/java/com/org/java/SpringbootDockerJenkinPipelineApplication.java
+++ src/main/java/com/org/java/SpringbootDockerJenkinPipelineApplication.java
@@ -2,12 +2,18 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDockerJenkinPipelineApplication {
+public class SpringbootDockerJenkinPipelineApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDockerJenkinPipelineApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootDockerJenkinPipelineApplication.class);
 	}
 
 }
